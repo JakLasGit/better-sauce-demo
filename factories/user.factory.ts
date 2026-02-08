19e8bb6d06defd458e17897496ab6ce9bd6e7996
Jakub <gitjaklas@gmail.com>
2026-02-08 18:53:11 +0100

    rethinked architecture - deleted builder, updated factory, updated actions to use password from env

--- factories/user.factory.ts
+++ factories/user.factory.ts
@@ -1,20 +1,45 @@
-import { UserBuilder } from '../builders/user.builder';
 import { UserData } from '../models/user.model';
 
 export class UserFactory {
+  private static getPassword(): string {
+    const password = process.env.SAUCE_PASSWORD;
+
+    if (!password) {
+      throw new Error('❌ FATAL ERROR: SAUCE_PASSWORD is not set in .env file!');
+    }
+
+    return password;
+  }
+
   static createStandardUser(): UserData {
-    return new UserBuilder().build();
+    return {
+      username: 'standard_user',
+      password: this.getPassword(),
+      role: 'standard',
+    };
   }
 
   static createLockedUser(): UserData {
-    return new UserBuilder().asLockedOut().build();
+    return {
+      username: 'locked_out_user',
+      password: this.getPassword(),
+      role: 'locked',
+    };
   }
 
   static createPerformanceGlitchUser(): UserData {
-    return new UserBuilder().asPerformanceGlitchUser().build();
+    return {
+      username: 'performance_glitch_user',
+      password: this.getPassword(),
+      role: 'performance_glitch',
+    };
   }
 
   static createInvalidUser(): UserData {
-    return new UserBuilder().asInvalidUser().build()
+    return {
+      username: `standard_user`,
+      password: 'wrong_password_123',
+      role: 'standard',
+    };
   }
 }
